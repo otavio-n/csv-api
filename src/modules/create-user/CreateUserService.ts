5f58c2334eaba3d2aae05a2fa0e56695b89ea1ef
Otávio Nishimura <otavio@integra.do>
2023-09-02 16:34:04 -0300

    add create user service

---
+++ src/modules/create-user/CreateUserService.ts
@@ -0,0 +1,27 @@
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../../repositories/IUsersRepositories";
+
+interface IUserBody {
+  name: string;
+  city: string;
+  country: string;
+  favorite_sport: string;
+}
+
+class CreateUserService {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  async execute({ name, city, country, favorite_sport }: IUserBody) {
+    const userAlreadyExists = await this.usersRepository.exists(name);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists!");
+    }
+
+    const user = User.create({ name, city, country, favorite_sport });
+    const userCreated = await this.usersRepository.create(user);
+    return userCreated;
+  }
+}
+
+export { CreateUserService };
