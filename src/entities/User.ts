f3132426b02050db854a333e095b6a4604cd3e8d
Otávio Nishimura <otavio@integra.do>
2023-09-02 16:32:55 -0300

    add user entity

---
+++ src/entities/User.ts
@@ -0,0 +1,23 @@
+class User {
+  id?: string;
+  name: string;
+  city: string;
+  country: string;
+  favorite_sport: string;
+
+  private constructor({ name, city, country, favorite_sport }: User) {
+    return Object.assign(this, {
+      name,
+      city,
+      country,
+      favorite_sport,
+    });
+  }
+
+  static create({ name, city, country, favorite_sport }: User) {
+    const user = new User({ name, city, country, favorite_sport });
+    return user;
+  }
+}
+
+export { User };
