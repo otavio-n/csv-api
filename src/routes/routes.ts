96837ea85691a591943d1a8f6eba2fe942c1a52a
Otávio Nishimura <otavio@integra.do>
2023-09-02 15:05:29 -0300

    first commit

---
+++ src/routes/routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const routes = Router();
+
+routes.post("/api/files", (request, response) =>
+);
+
+routes.get("/api/users", (request, response) =>
+);
+
+export { routes };
